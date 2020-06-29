628820cd603d0d1d8006cf2fefa277712e9831df
gaoxin <380588773@qq.com>
2020-06-29 17:11:37 +0800

    第一次提交，只写了入门级案例，只举例了直连式交换机

---
+++ rabbitmq-provider/src/main/java/com/gx/rabbitmqprovider/controller/SendMessageController.java
@@ -0,0 +1,33 @@
+package com.gx.rabbitmqprovider.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class SendMessageController {
+
+    //使用RabbitTemplate,这提供了接收/发送等等方法
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @RequestMapping("/sendDirectMessage")
+    public String sendDirectMessage(){
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "test message, hello!";
+        String creatTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String,Object> map = new HashMap<>();
+        map.put("messageId",messageId);
+        map.put("messageData",messageData);
+        map.put("createTime",creatTime);
+        //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
+        rabbitTemplate.convertAndSend("TestDirectExchange","TestDirectRouting",map);
+        return "ok";
+    }
+}
